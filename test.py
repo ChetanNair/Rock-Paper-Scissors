53f0a18824197a415acaf28850b3f3c2d5eff0f3
Chetan Nair <cnair@stanford.edu>
2022-01-01 21:33:04 +0400

    Counts and displays number of fingers

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -23,19 +24,46 @@ with mp_hands.Hands(
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        handNumber = 0
         count = 0
+        hand_landmarks = []
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
+            for hand in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     image,
-                    hand_landmarks,
+                    hand,
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
 
+                label = results.multi_handedness[handNumber].classification[0].label
+                for id, landmark in enumerate(hand.landmark):
+                    imgH, imgW, imgC = image.shape  # height, width, channel for image
+                    xPos, yPos = int(landmark.x *
+                                     imgW), int(landmark.y * imgH)
+                    hand_landmarks.append([id, xPos, yPos, label])
+
+                # Index Finger
+                if hand_landmarks[8][2] < hand_landmarks[6][2]:
+                    count += 1
+                # Middle Finger
+                if hand_landmarks[12][2] < hand_landmarks[10][2]:
+                    count += 1
+                # Ring Finger
+                if hand_landmarks[16][2] < hand_landmarks[14][2]:
+                    count += 1
+                # Pinky Finger
+                if hand_landmarks[20][2] < hand_landmarks[18][2]:
+                    count += 1
+                # Thumb
+                if hand_landmarks[4][3] == "Left" and hand_landmarks[4][1] > hand_landmarks[3][1]:
+                    count += 1
+                elif hand_landmarks[4][3] == "Right" and hand_landmarks[4][1] < hand_landmarks[3][1]:
+                    count += 1
+                handNumber += 1
         # Flip the image horizontally for a selfie-view display.
         image = cv2.flip(image, 1)
-        cv2.putText(image, "NumOfFingers", (45, 375),
+        cv2.putText(image, str(count), (45, 375),
                     cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 10)
         cv2.imshow('Rock, Paper, Scissors', image)
         if cv2.waitKey(1) & 0xFF == 27:
